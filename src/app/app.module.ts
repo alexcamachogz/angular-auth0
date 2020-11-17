589818261b6d4d4f377b1155fd083110de0a4efb
Alejandra Camacho <camacho.ale94@gmail.com>
2020-11-17 17:56:04 -0600

    SDK auth0 installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NabvarComponent } from './components/nabvar/nabvar.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProtectedComponent } from './components/protected/protected.component';
-import { PricesComponent } from './components/prices/prices.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NabvarComponent } from './components/nabvar/nabvar.component'
+import { HomeComponent } from './components/home/home.component'
+import { ProtectedComponent } from './components/protected/protected.component'
+import { PricesComponent } from './components/prices/prices.component'
+
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular'
 
 @NgModule({
   declarations: [
@@ -18,9 +21,13 @@ import { PricesComponent } from './components/prices/prices.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'alexcamachogz.us.auth0.com',
+      clientId: 'CaZqoWGh6gWfgKcClPfEeepbaabfE3nh'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
